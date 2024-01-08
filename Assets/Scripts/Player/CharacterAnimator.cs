2741145fe61fb98eeae628471903561adaf4618b
Jose Lopez <jdloci05@gmail.com>
2024-01-08 14:17:15 -0500

    updateCharacters
    changed The animations of the characters

--- Assets/Scripts/Player/CharacterAnimator.cs
+++ Assets/Scripts/Player/CharacterAnimator.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CharacterAnimator : MonoBehaviour
 {
-    [Header("Default and NPC Skin")]
+    [Header("Default or NPC Skin")]
     [SerializeField] List<Sprite> defaultWalkDownSprites;
     [SerializeField] List<Sprite> defaultWalkUpSprites;
     [SerializeField] List<Sprite> defaultWalkRightSprites;
@@ -29,19 +29,10 @@ public class CharacterAnimator : MonoBehaviour
     [SerializeField] List<Sprite> skin4WalkRightSprites;
     [SerializeField] List<Sprite> skin4WalkLeftSprites;
 
-    [Header("Skin 5")]
-    [SerializeField] List<Sprite> skin5WalkDownSprites;
-    [SerializeField] List<Sprite> skin5WalkUpSprites;
-    [SerializeField] List<Sprite> skin5WalkRightSprites;
-    [SerializeField] List<Sprite> skin5WalkLeftSprites;
-
-    [Header("Skin 6")]
-    [SerializeField] List<Sprite> skin6WalkDownSprites;
-    [SerializeField] List<Sprite> skin6WalkUpSprites;
-    [SerializeField] List<Sprite> skin6WalkRightSprites;
-    [SerializeField] List<Sprite> skin6WalkLeftSprites;
-
     [SerializeField] List<Sprite> defaultSkin;
+
+    [SerializeField] bool IsPlayer = false;
+    private bool Right;
 
     // Parameters
 
@@ -72,17 +63,6 @@ public class CharacterAnimator : MonoBehaviour
     SpriteAnimator skin4WalkRightAnim;
     SpriteAnimator skin4WalkLeftAnim;
 
-    SpriteAnimator skin5WalkDownAnim;
-    SpriteAnimator skin5WalkUpAnim;
-    SpriteAnimator skin5WalkRightAnim;
-    SpriteAnimator skin5WalkLeftAnim;
-
-    SpriteAnimator skin6WalkDownAnim;
-    SpriteAnimator skin6WalkUpAnim;
-    SpriteAnimator skin6WalkRightAnim;
-    SpriteAnimator skin6WalkLeftAnim;
-
-
     SpriteAnimator currentAnim;
     bool wasPreviouslyMoving;
 
@@ -112,78 +92,120 @@ public class CharacterAnimator : MonoBehaviour
         skin4WalkRightAnim = new SpriteAnimator(skin4WalkRightSprites, spriteRenderer);
         skin4WalkLeftAnim = new SpriteAnimator(skin4WalkLeftSprites, spriteRenderer);
 
-        skin5WalkDownAnim = new SpriteAnimator(skin5WalkDownSprites, spriteRenderer);
-        skin5WalkUpAnim = new SpriteAnimator(skin5WalkUpSprites, spriteRenderer);
-        skin5WalkRightAnim = new SpriteAnimator(skin5WalkRightSprites, spriteRenderer);
-        skin5WalkLeftAnim = new SpriteAnimator(skin5WalkLeftSprites, spriteRenderer);
-
-        skin6WalkDownAnim = new SpriteAnimator(skin6WalkDownSprites, spriteRenderer);
-        skin6WalkUpAnim = new SpriteAnimator(skin6WalkUpSprites, spriteRenderer);
-        skin6WalkRightAnim = new SpriteAnimator(skin6WalkRightSprites, spriteRenderer);
-        skin6WalkLeftAnim = new SpriteAnimator(skin6WalkLeftSprites, spriteRenderer);
-
-
         currentAnim = new SpriteAnimator(defaultSkin, spriteRenderer);
     }
 
     private void Update()
     {
         var prevAnim = currentAnim;
+        if (IsPlayer)
+        {
+            if (tag == "Player" && MoveX == 1)
+            {
+                Right = true;
+                currentAnim = defaultWalkRightAnim;
+            }                
+            else if (tag == "Player" && MoveX == -1)
+            {
+                Right = false;
+                currentAnim = defaultWalkLeftAnim;
+            }
+            else if (tag == "Player" && MoveY == 1 || tag == "Player" && MoveY == -1)
+            {
+                if (Right)
+                {
+                    currentAnim = defaultWalkRightAnim;
+                }
+                else
+                {
+                    currentAnim = defaultWalkLeftAnim;
+                }
+            }
 
-        if ( tag == "Player" && MoveX == 1)
-            currentAnim = defaultWalkRightAnim;
-        else if (tag == "Player" && MoveX == -1)
-            currentAnim = defaultWalkLeftAnim;
-        else if (tag == "Player" && MoveY == 1)
-            currentAnim = defaultWalkUpAnim;
-        else if (tag == "Player" && MoveY == -1)
-            currentAnim = defaultWalkDownAnim;
+            else if (tag == "Skin 2" && MoveX == 1)
+            {
+                Right = true;
+                currentAnim = skin2WalkRightAnim;
+            }              
+            else if (tag == "Skin 2" && MoveX == -1)
+            {
+                Right = false;
+                currentAnim = skin2WalkLeftAnim;
+            }
+                
+            else if (tag == "Skin 2" && MoveY == 1 || tag == "Skin 2" && MoveY == -1)
+            {
+                if (Right)
+                {
+                    currentAnim = skin2WalkRightAnim;
+                }
+                else
+                {
+                    currentAnim = skin2WalkLeftAnim;
+                }
+            }
 
-        else if (tag == "Skin 2" && MoveX == 1)
-            currentAnim = skin2WalkRightAnim;
-        else if (tag == "Skin 2" && MoveX == -1)
-            currentAnim = skin2WalkLeftAnim;
-        else if (tag == "Skin 2" && MoveY == 1)
-            currentAnim = skin2WalkUpAnim;
-        else if (tag == "Skin 2" && MoveY == -1)
-            currentAnim = skin2WalkDownAnim;
+            else if (tag == "Skin 3" && MoveX == 1)
+            {
+                Right = true;
+                currentAnim = skin3WalkRightAnim;
+            }
+                
+            else if (tag == "Skin 3" && MoveX == -1)
+            {
+                Right = false;
+                currentAnim = skin3WalkLeftAnim;
+            }
+                
+            else if (tag == "Skin 3" && MoveY == 1 || tag == "Skin 3" && MoveY == -1)
+            {
+                if (Right)
+                {
+                    currentAnim = skin3WalkRightAnim;
+                }
+                else
+                {
+                    currentAnim = skin3WalkLeftAnim;
+                }
+            }
 
-        else if (tag == "Skin 3" && MoveX == 1)
-            currentAnim = skin3WalkRightAnim;
-        else if (tag == "Skin 3" && MoveX == -1)
-            currentAnim = skin3WalkLeftAnim;
-        else if (tag == "Skin 3" && MoveY == 1)
-            currentAnim = skin3WalkUpAnim;
-        else if (tag == "Skin 3" && MoveY == -1)
-            currentAnim = skin3WalkDownAnim;
+            else if (tag == "Skin 4" && MoveX == 1)
+            {
+                Right = true;
+                currentAnim = skin4WalkRightAnim;
+            }
+                
+            else if (tag == "Skin 4" && MoveX == -1)
+            {
+                Right = false;
+                currentAnim = skin4WalkLeftAnim;
+            }
+                
+            else if (tag == "Skin 4" && MoveY == 1 || tag == "Skin 4" && MoveY == -1)
+            {
+                if (Right)
+                {
+                    currentAnim = skin4WalkRightAnim;
+                }
+                else
+                {
+                    currentAnim = skin4WalkLeftAnim;
+                }
 
-        else if (tag == "Skin 4" && MoveX == 1)
-            currentAnim = skin4WalkRightAnim;
-        else if (tag == "Skin 4" && MoveX == -1)
-            currentAnim = skin4WalkLeftAnim;
-        else if (tag == "Skin 4" && MoveY == 1)
-            currentAnim = skin4WalkUpAnim;
-        else if (tag == "Skin 4" && MoveY == -1)
-            currentAnim = skin4WalkDownAnim;
-
-        else if (tag == "Skin 5" && MoveX == 1)
-            currentAnim = skin5WalkRightAnim;
-        else if (tag == "Skin 5" && MoveX == -1)
-            currentAnim = skin5WalkLeftAnim;
-        else if (tag == "Skin 5" && MoveY == 1)
-            currentAnim = skin5WalkUpAnim;
-        else if (tag == "Skin 5" && MoveY == -1)
-            currentAnim = skin5WalkDownAnim;
-
-        else if (tag == "Skin 6" && MoveX == 1)
-            currentAnim = skin6WalkRightAnim;
-        else if (tag == "Skin 6" && MoveX == -1)
-            currentAnim = skin6WalkLeftAnim;
-        else if (tag == "Skin 6" && MoveY == 1)
-            currentAnim = skin6WalkUpAnim;
-        else if (tag == "Skin 6" && MoveY == -1)
-            currentAnim = skin6WalkDownAnim;
-
+            }
+        }
+        else
+        {
+            if (tag == "Player" && MoveX == 1)
+                currentAnim = defaultWalkRightAnim;
+            else if (tag == "Player" && MoveX == -1)
+                currentAnim = defaultWalkLeftAnim;
+            else if (tag == "Player" && MoveY == 1)
+                currentAnim = defaultWalkUpAnim;
+            else if (tag == "Player" && MoveY == -1)
+                currentAnim = defaultWalkDownAnim;         
+        }
+        
         if (currentAnim != prevAnim || IsMoving != wasPreviouslyMoving)
             currentAnim.Start();
 
